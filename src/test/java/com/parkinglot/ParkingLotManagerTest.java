4c05630df7c878231d0e30bb3314896fed446380
vincent <luk96515@gmail.com>
2021-10-26 20:45:20 +0800

    [ParkingLot][Vincent] feat: complete manager test case AC2

--- src/test/java/com/parkinglot/ParkingLotManagerTest.java
+++ src/test/java/com/parkinglot/ParkingLotManagerTest.java
@@ -1,11 +1,12 @@
 package com.parkinglot;
 
+import com.parkinglot.exception.ParkingLotIsFullException;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Stack;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingLotManagerTest {
     @Test
@@ -64,6 +65,55 @@ class ParkingLotManagerTest {
         assertNotNull(ticket);
     }
 
+    @Test//AC2
+    void should_return_ticket_when_execute_assignParkTask_given_a_parking_manager_and_a_car(){
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot(10);
+        ParkingLotManager parkingLotManager = new ParkingLotManager(parkingLot);
+        parkingLotManager.addParkingBoy(parkingLotManager);
+        //when
+        Ticket ticket = parkingLotManager.assignParkTask(car, parkingLotManager);
+
+        //then
+        assertNotNull(ticket);
+    }
+
+    @Test//AC2
+    void should_return_car_when_execute_assignParkTask_given_a_parking_manager_and_a_ticket(){
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot(10);
+        ParkingLotManager parkingLotManager = new ParkingLotManager(parkingLot);
+        parkingLotManager.addParkingBoy(parkingLotManager);
+
+        Ticket ticket = parkingLotManager.assignParkTask(car, parkingLotManager);
+
+        //when
+        Car actualCar = parkingLotManager.assignFetchTask(ticket,parkingLotManager);
+        //then
+        assertNotNull(ticket);
+    }
+
+    @Test //AC3
+    void should_error_msg_when_execute_assignParkTask_given_a_parkingboy_and_a_car_and_a_full_parkingLot(){
+        //given
+        Car car = new Car();
+        ParkingLot parkingLot = new ParkingLot(1);
+        StandardParkingBoy parkingBoy = new SmartParkingBoy(parkingLot);
+        ParkingLotManager parkingLotManager = new ParkingLotManager(parkingLot);
+        parkingLotManager.addParkingBoy(parkingBoy);
+        parkingLotManager.assignParkTask(car, parkingBoy);
+
+
+        //when
+        ParkingLotIsFullException parkingLotIsFullException = assertThrows(
+                ParkingLotIsFullException.class, () -> parkingLotManager.assignParkTask(car,parkingBoy));
+
+
+        //then
+        assertEquals("No available position", parkingLotIsFullException.getMessage());
+    }
 
 
 
